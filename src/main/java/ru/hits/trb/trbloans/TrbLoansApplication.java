2fce51a4e88e24a6bf4205241cf22e964ce4278d
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-02-21 16:55:06 +0700

    Инициализация проекта

---
+++ src/main/java/ru/hits/trb/trbloans/TrbLoansApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.trb.trbloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrbLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrbLoansApplication.class, args);
+	}
+
+}
