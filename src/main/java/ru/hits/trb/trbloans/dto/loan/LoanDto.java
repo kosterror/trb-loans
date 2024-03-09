2049d230b8eb109576448dc8258999f1966cad0e
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-03-09 20:02:59 +0700

    Поправил формат даты

--- src/main/java/ru/hits/trb/trbloans/dto/loan/LoanDto.java
+++ src/main/java/ru/hits/trb/trbloans/dto/loan/LoanDto.java
@@ -1,5 +1,7 @@
 package ru.hits.trb.trbloans.dto.loan;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.hits.trb.trbloans.dto.tariff.TariffDto;
 import ru.hits.trb.trbloans.entity.enumeration.LoanState;
@@ -14,8 +16,12 @@ public class LoanDto {
 
     private UUID id;
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    @Schema(type = "number")
     private Date issuedDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    @Schema(type = "number")
     private Date repaymentDate;
 
     private long issuedAmount;
--- src/main/java/ru/hits/trb/trbloans/dto/loan/LoanRepaymentDto.java
+++ src/main/java/ru/hits/trb/trbloans/dto/loan/LoanRepaymentDto.java
@@ -1,6 +1,8 @@
 package ru.hits.trb.trbloans.dto.loan;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.hits.trb.trbloans.entity.enumeration.LoanRepaymentState;
 
@@ -12,6 +14,8 @@ public class LoanRepaymentDto {
 
     private UUID id;
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    @Schema(type = "number")
     private Date date;
 
     private long amount;
